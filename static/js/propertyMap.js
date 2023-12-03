09c967eb5b7f93f358147df7c439831a6ba88611
Brecht Nys <123317735+NysB@users.noreply.github.com>
2023-12-03 18:35:28 -0500

    Update

--- static/js/propertyMap.js
+++ static/js/propertyMap.js
@@ -40,104 +40,62 @@ function updateMap(data) {
     marker.bindPopup(`
       <strong>${property.propertyType}</strong><br>
       <img src="${image}" alt="Property Image" width="150"><br>
-      Price: $${property.price}
+      Price: €${property.purchasePrice}
     `).openPopup();
 
     marker.on('click', () => {
       document.getElementById('selected-property-info').innerHTML = `
         <h3>Economics</h3>
-        <p id="price">Price: €${property.price}</p>
-        <p id="predicted-rent">Predicted Monthly Rent (LT): €${property.predictedRentPrice}</p>
-        <h3>Description</h3>
-        <p id="property-type">Property Type: ${property.propertyType}</p>
-        <p id="bedrooms">Bedrooms: ${property.bedrooms}</p>
-        <p id="bathrooms">Bathrooms: ${property.bathrooms}</p>
-        <p id="size">Size: ${property.size}</p>
-        <p id="status">Status: ${property.status}</p>
-        <p id="floor">Floor: ${property.floor}</p>
-        <p id="has-lift">Has Lift: ${property.hasLift}</p>
-        <p id="new-development">New Development: ${property.newDevelopment}</p>
-        <p id="property-size">Size: ${property.size}m2</p>
-        <p id="property-url">URL: <a href="${property.URL}" target="_blank">${property.URL}</a></p>
-        <div id="roi-chart-container">
-          <canvas id="roi-chart" width="400" height="400"></canvas>
+        <div class="horizontal-container">
+          <div class="horizontal-property-info-block">
+            <p id="price">Price: € ${property.purchasePrice}</p>
+          </div>
+          <div class="horizontal-property-info-block">
+            <p id="predicted-rent">Predicted Monthly Rent (LT): € ${property.predictedMonthlyRent}</p>
+          </div>
         </div>
+        <h3>Description</h3>
+          <div class="horizontal-container">
+            <p id="description">${property.description}</p>
+          </div>
+          <div class="horizontal-container">
+            <div class="horizontal-property-info-block">
+              <p id="property-type">Property Type: ${property.propertyType}</p>
+            </div>
+            <div class="horizontal-property-info-block">
+              <p id="size">Size: ${property.size}m2</p>
+            </div>
+          </div>  
+          <div class="horizontal-container">
+            <div class="horizontal-property-info-block">
+              <p id="bathrooms">Bedrooms: ${property.bedrooms}</p>
+            </div>
+            <div class="horizontal-property-info-block">
+              <p id="bathrooms">Bathrooms: ${property.bathrooms}</p>
+            </div>
+          </div>
+          <div class="horizontal-container">
+            <div class="horizontal-property-info-block">
+              <p id="status">Status: ${property.status}</p>
+            </div>
+            <div class="horizontal-property-info-block">
+              <p id="new-development">New Development: ${property.newDevelopment}</p>
+            </div>
+          </div>   
+          <div class="horizontal-container">
+            <div class="horizontal-property-info-block">
+              <p id="floor">Floor: ${property.floor}</p>
+            </div>
+            <div class="horizontal-property-info-block">
+              <p id="has-lift">Has Lift: ${property.hasLift}</p>
+            </div>
+          </div>   
+          <div class="horizontal-container">
+            <div class="horizontal-property-info-block">
+              <p id="property-url">URL: <a href="${property.URL}" target="_blank">${property.URL}</a></p>
+            </div>
+          </div>        
       `;
-
-      // Calculate and display ROI
-      calculateAndDisplayROI(property);
     });
   });
 }
-
-// Function to calculate Return on Investment (ROI)
-function calculateROI(ltv, mortgageRate, mortgageTenor) {
-  // You can add your ROI calculation logic here
-  const investment = 100000; // Replace with your actual investment amount
-  const rentIncome = 12000; // Replace with your annual rent income
-
-  const mortgageAmount = investment * (ltv / 100);
-  const interestExpense = (mortgageAmount * mortgageRate) / 100;
-  const totalExpenses = interestExpense;
-  const netIncome = rentIncome - totalExpenses;
-
-  const roi = (netIncome / investment) * 100;
-
-  return roi.toFixed(2);
-}
-
-// Function to display ROI in a chart
-function displayROIChart(ltv, mortgageRate, mortgageTenor) {
-  const roi = calculateROI(ltv, mortgageRate, mortgageTenor);
-
-  const roiChartCanvas = document.getElementById('roi-chart');
-  const roiChart = new Chart(roiChartCanvas, {
-    type: 'bar',
-    data: {
-      labels: ['Return on Investment (%)'],
-      datasets: [
-        {
-          label: 'ROI',
-          data: [roi],
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
-}
-
-// Add an event listener to the form to handle ROI calculation
-document.querySelector('#filter-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  // Get the form input values inside the event handler
-  const ltv = parseFloat(document.querySelector('#ltv').value);
-  const mortgageRate = parseFloat(document.querySelector('#mortgage-rate').value);
-  const mortgageTenor = parseInt(document.querySelector('#mortgage-tenor').value);
-
-  // Fetch filtered data from the server
-  fetch('/filter', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: `ltv=${ltv}&mortgage-rate=${mortgageRate}&mortgage-tenor=${mortgageTenor}`,
-  })
-    .then(response => response.json())
-    .then(data => {
-      updateMap(data); // Update the map with filtered data
-      displayROIChart(ltv, mortgageRate, mortgageTenor);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-});
