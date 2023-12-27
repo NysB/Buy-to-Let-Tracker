930712721ffdd7c35d1faf581051865a4e899ee1
Brecht Nys <123317735+NysB@users.noreply.github.com>
2023-12-27 10:12:36 -0500

    Update

--- static/js/historicalGraph.js
+++ static/js/historicalGraph.js
@@ -1,41 +1,54 @@
-// Function to update the chart based on selected city
-function updateChart() {
-    const selectedCity = document.getElementById('citySelect').value;
-    const cityData = data.filter(item => item.city === selectedCity);
-  
-    // Clear previous chart
-    const chartContainer = document.getElementById('chart');
-    chartContainer.innerHTML = '';
-  
-    // Create bars for each attribute
-    cityData.forEach(item => {
-      const bar = document.createElement('div');
-      bar.className = 'bar';
-      // Adjust height as needed (dividing by 1000 might be too much, consider adjusting)
-      bar.style.height = `${item.fifty / 1000}px`;
-      bar.innerText = `${item.attribute} - ${item.fifty.toFixed(2)}`;
-      chartContainer.appendChild(bar);
-    });
-  }
-  
-  // Function to fetch data from the server and create the graphs
-  function historicalGraph() {
-    fetch('/historicalPurchaseData')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        // Call the updateChart function with the fetched data
-        updateChart(data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }
-  
-  // Call the historicalGraph function
-  historicalGraph();
-  
+// Sample data
+const data = [
+    { group: 'Group A', date: '2023-01-01', value: 10 },
+    { group: 'Group A', date: '2023-01-02', value: 15 },
+    { group: 'Group A', date: '2023-01-03', value: 20 },
+    { group: 'Group A', date: '2023-01-04', value: 25 },
+    { group: 'Group A', date: '2023-01-05', value: 30 },
+    { group: 'Group B', date: '2023-01-01', value: 8 },
+    { group: 'Group B', date: '2023-01-02', value: 12 },
+    { group: 'Group B', date: '2023-01-03', value: 18 },
+    { group: 'Group B', date: '2023-01-04', value: 22 },
+    { group: 'Group B', date: '2023-01-05', value: 28 },
+];
+
+// Extract unique dates and groups
+const uniqueDates = [...new Set(data.map(item => item.date))];
+const uniqueGroups = [...new Set(data.map(item => item.group))];
+
+// Define custom background colors for each date
+const backgroundColors = ['#FF5733', '#33FF57', '#5733FF', '#FFFF33', '#33FFFF'];
+
+// Prepare data for Chart.js
+const chartData = {
+    labels: uniqueGroups, // Reverse the order to show groups on X-axis
+    datasets: uniqueDates.map((date, index) => ({
+        label: date,
+        data: uniqueGroups.map(group => {
+            const item = data.find(entry => entry.group === group && entry.date === date);
+            return item ? item.value : 0;
+        }),
+        backgroundColor: backgroundColors[index], // Use custom background color
+        borderColor: 'rgba(0, 0, 0, 1)',
+        borderWidth: 1,
+    })),
+};
+
+// Get the canvas element
+const ctx = document.getElementById('myChart').getContext('2d');
+
+// Create the bar chart
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: chartData,
+    options: {
+        scales: {
+            x: {
+                stacked: false,
+            },
+            y: {
+                stacked: false,
+            },
+        },
+    },
+});
