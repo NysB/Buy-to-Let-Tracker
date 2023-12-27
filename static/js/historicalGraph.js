9d45a96c77a4611b4ff4917dc4ebc28bc1230afd
Brecht Nys <123317735+NysB@users.noreply.github.com>
2023-12-27 17:02:20 -0500

    Update

--- static/js/historicalGraph.js
+++ static/js/historicalGraph.js
@@ -2,13 +2,14 @@
 // Declare the data variable
 let data;
 
+
 // Get the canvas element
-const ctx = document.getElementById('myChart').getContext('2d');
+let ctx = document.getElementById('myChart').getContext('2d');
 
 // Create the initial bar chart
-const myChart = new Chart(ctx, {
+let myChart = new Chart(ctx, {
     type: 'bar',
-    data: {},  // Set initial data as an empty object
+    data: {},  
     options: {
         scales: {
             x: {
@@ -21,54 +22,72 @@ const myChart = new Chart(ctx, {
     },
 });
 
+
 // Function to update the chart based on selected city
 function updateChart() {
-    // Move this line inside the function
-    const uniqueDates = [...new Set(data.map(item => item.date))];
-
+    let selectedCity = document.getElementById('citySelect').value;
+    let uniqueDates = [...new Set(data.map((item) => item.date))];
+  
+    console.log('updateChart: selectedCity:', selectedCity);
     console.log('updateChart: uniqueDates:', uniqueDates);
-
-    // Extract unique groups and cities
-    const uniqueGroups = ['zeroBedroom', 'oneBedroom', 'twoBedroom', 'threeBedroom', 'fourBedroom', 'fiveBedroom', 'moreThanFiveBedroom', 'twentyFive', 'fifty', 'seventyFive', 'hundred', 'hundredFifty', 'twoHundred', 'moreThanTwoHundred'];
-    const uniqueCities = [...new Set(data.map(item => item.city))];
-
+  
+    // Extract unique groups
+    let uniqueGroups = [
+      'zeroBedroom',
+      'oneBedroom',
+      'twoBedroom',
+      'threeBedroom',
+      'fourBedroom',
+      'fiveBedroom',
+      'moreThanFiveBedroom',
+      'twentyFive',
+      'fifty',
+      'seventyFive',
+      'hundred',
+      'hundredFifty',
+      'twoHundred',
+      'moreThanTwoHundred',
+    ];
+  
     console.log('updateChart: uniqueGroups:', uniqueGroups);
-    console.log('updateChart: uniqueCities:', uniqueCities);
-
+  
     // Define custom background colors for each date
-    const backgroundColors = ['#FF5733', '#33FF57', '#5733FF', '#FFFF33', '#33FFFF'];
-
+    let backgroundColors = ['#FF5733', '#33FF57', '#5733FF', '#FFFF33', '#33FFFF'];
+  
     // Prepare data for Chart.js
-    const chartData = {
-        labels: uniqueGroups,
-        datasets: uniqueDates.map((date, index) => ({
-            label: date,
-            data: uniqueGroups.map(group => {
-                // Filter data for the specific group, city, and date
-                const groupData = data.filter(entry => entry.city === group);
-                const cityData = groupData.filter(entry => new Date(entry.date).getTime() === new Date(date).getTime());
-                // Assuming you want to sum the values for the specified city and group on that date
-                const sum = cityData.reduce((accumulator, entry) => accumulator + entry.value, 0);
-                return sum;
-            }),
-            backgroundColor: backgroundColors[index % backgroundColors.length],
-            borderColor: 'rgba(0, 0, 0, 1)',
-            borderWidth: 1,
-        })),
+    let chartData = {
+      labels: uniqueGroups,
+      datasets: uniqueDates.map((date, index) => ({
+        label: date,
+        data: uniqueGroups.map((group) => {
+          
+          let groupData = data.filter((entry) => entry.group === group && entry.city === selectedCity);
+          let cityData = groupData.filter(
+            (entry) => new Date(entry.date).getTime() === new Date(date).getTime()
+          );
+          
+          let sum = cityData.reduce((accumulator, entry) => accumulator + entry.value, 0);
+          return sum;
+        }),
+        backgroundColor: backgroundColors[index % backgroundColors.length],
+        borderColor: 'rgba(0, 0, 0, 1)',
+        borderWidth: 1,
+      })),
     };
-
+  
     // Update the existing chart data
     myChart.data = chartData;
-
+  
     console.log('updateChart: chartData:', chartData);
-
+  
     // Update the chart
     myChart.update();
-}
+  }
+
 
 // Function to fetch data from the server and create the graphs
 function historicalGraph() {
-    fetch('/historicalRentData')
+    fetch('/historicalPurchaseData')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -77,7 +96,30 @@ function historicalGraph() {
         })
         .then(responseData => {
             // Assign the retrieved data to the data variable
-            data = responseData;
+            data = [];
+
+            console.log('historicalGraph: responseData:', responseData);
+
+            responseData.forEach((entry) => {
+                const city = entry.city;
+                const date = new Date(entry.date).toISOString().split('T')[0];
+              
+                Object.keys(entry).forEach((key) => {
+                  if (key !== 'attribute' && key !== 'city' && key !== 'date') {
+                    const group = key;
+                    const value = entry[key];
+              
+                    data.push({
+                      city: city,
+                      group: group,
+                      date: date,
+                      value: value,
+                    });
+                  }
+                });
+              });
+            
+            console.log('historicalGraph: data:', data);
 
             // Call the updateChart function with the fetched data
             updateChart();
